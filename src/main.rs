ebc67a1841160ceb5ab93dc9320a24d2d5052a73
Ryan Duffendack <ryan.duffendack@gmail.com>
2023-09-06 19:02:58 -0500

    Finished tech demo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 #[macro_use] extern crate rocket;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/goodbye")]
+fn goodbye() -> &'static str {
+    "Goodbye World!"
+}
+
+#[get("/time/now")]
+fn get_time() -> Option<String> {
+    Some(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs().to_string())
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, goodbye, get_time])
 }
