b4bad6aa73ef555413291503d12d18b0c35ce42f
Corgwn <ryan.duffendack@gmail.com>
2023-10-28 23:38:54 -0500

    added draft of data structures

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,46 @@ use rocket::serde::json::Json;
 use serde::{Deserialize, Serialize};
 use std::time::{SystemTime, UNIX_EPOCH};
 
-#[derive(Deserialize)]
+//Data Structs
+
+#[derive(Serialize, Deserialize)]
 struct ClassInfo {
-    name: String,
     id: u32,
+    name: String,
+    min_units: u32,
+    max_units: u32,
+    subject: String,
+    catalog_num: u32,
+    req_group: u32,
     description: String,
 }
 
 #[derive(Serialize, Deserialize)]
+struct Semester {
+    classes: Vec<u32>,
+    semester: u32,
+    term: String,
+}
+
+#[derive(Serialize, Deserialize)]
 struct Schedule {
-    classes: u8,
+    classes: Vec<Semester>,
     valid: bool,
 }
 
+#[derive(Serialize, Deserialize)]
+struct ReqGroup {
+    id: u32,
+    effective_date: String,
+    name: String,
+    classes: Vec<u32>,
+    union: bool,
+}
+
 //Real API paths
+
 #[get("/get_search_results/<input_string>")]
-fn get_search_results(input_string: String) -> Json<String> {
+fn get_search_results(input_string: String) -> Json<Vec<ClassInfo>> {
     // TODO: Connect to database
 
     // TODO: Get list of all class names and numbers
@@ -29,29 +53,29 @@ fn get_search_results(input_string: String) -> Json<String> {
 
     // TODO: Return filtered list like { class_id: class_name }
 
-    return Json(input_string);
+    todo!()
 }
 
 #[get("/get_class_information/<class_id>")]
-fn get_class_information(class_id: u32) -> Json<String> {
+fn get_class_information(class_id: u32) -> Json<ClassInfo> {
     // TODO: Connect to database
 
     // TODO: Get all information relating to given class_id
 
     // TODO: Return information as JSON
 
-    return Json(class_id.to_string());
+    todo!()
 }
 
 #[post("/validate", format = "application/json", data = "<schedule>")]
-fn validate_schedule(schedule: Json<Schedule>) -> Option<String> {
+fn validate_schedule(schedule: Json<Schedule>) -> Json<bool> {
     // TODO: Connect to database
 
     // TODO: For each class in the schedule, ensure that all prereqs were taken before it
 
     // TODO: Return the result
 
-    Some("true".to_string())
+    todo!()
 }
 
 // Example API paths
